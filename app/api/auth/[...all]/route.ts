cfb637379b3eb30633c14888da99fa2679ac955e
vansh317 <vanshahuja317@gmail.com>
2025-11-22 13:47:30 +0530

    changed betterauth runtime

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,3 +1,7 @@
+export const runtime = "nodejs";
+export const preferredRegion = "iad1";
+export const dynamic = "force-dynamic";
+
 import { auth } from "@/lib/auth"; // path to your auth file
 import { toNextJsHandler } from "better-auth/next-js";
 export const { POST, GET } = toNextJsHandler(auth);
